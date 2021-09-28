b78a6573406ac1cdb8b7397d9ffe14b2f48c06b7
Alina200207 <alina_m2002@list.ru>
2021-09-28 09:14:20 +0500

    'Задача1_версия1.0'

--- src/Bot.java
+++ src/Bot.java
@@ -2,47 +2,37 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 
-/**
- * Класс-обработчик поступающих к боту сообщений.
- */
 public class Bot extends TelegramLongPollingBot {
-    /**
-     * Метод, который возвращает токен, выданный нам ботом @BotFather.
-     * @return токен
-     */
+
     @Override
     public String getBotToken() {
         return "1983849094:AAEDDaWbNZTadHWRfyVrr6nMups8zRk1CNI";
     }
 
-    /**
-     * Метод-обработчик поступающих сообщений.
-     * @param update объект, содержащий информацию о входящем сообщении
-     */
+    @Override
+    public String getBotUsername() {
+        return "@Pump_up_your_brain_bot";
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
-        SendMessage sendMessage=new SendMessage();
-        if(update.getMessage()!=null && update.getMessage().hasText())
+        if(update.getMessage()!=null && update.getMessage().hasText() && update.getMessage().getText().equals("/help"))
         {
             Long chat_Id = update.getMessage().getChatId();
+            var help = new Help();
             try{
-                execute(new SendMessage(chat_Id.toString(),"Hello"));
+                execute(new SendMessage(chat_Id.toString(), help.giveHelp()));
+
             }
             catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    /**
-     * Метод, который возвращает имя пользователя бота.
-     * @return имя пользователя
-     */
-    @Override
-    public String getBotUsername() {
-        return "@Pump_up_your_brain_bot";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 
 public class Main {
     public static void main(String[] args) {
+
         try{
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new Bot());
