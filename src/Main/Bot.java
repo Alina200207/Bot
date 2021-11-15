d6407e54363108365d9c24a470b6181ee7d078b6
Alina200207 <alina_m2002@list.ru>
2021-11-15 16:24:06 +0500

    Задача3

--- src/Main/Bot.java
+++ src/Main/Bot.java
@@ -1,13 +1,18 @@
 package Main;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
+import org.telegram.telegrambots.meta.generics.TelegramBot;
+
+import java.util.*;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -20,15 +25,15 @@ public class Bot extends TelegramLongPollingBot {
     };
 
     private Map<String, UserData> UsersData = new HashMap<>();
-    private IssueCommand issueCommand = new IssueCommand();
-    private SequenceCommand sequenceCommand = new SequenceCommand();
-    private ExamplesCommand examplesCommand = new ExamplesCommand();
-    private LevelCommand levelCommand = new LevelCommand();
-    private Statistic statistic = new Statistic();
+    private final IssueCommand issueCommand = new IssueCommand();
+    private final SequenceCommand sequenceCommand = new SequenceCommand();
+    private final ExamplesCommand examplesCommand = new ExamplesCommand();
+    private final LevelCommand levelCommand = new LevelCommand();
+    private final Statistic statistic = new Statistic();
 
     @Override
     public String getBotToken() {
-        return "1983849094:AAHsoOtc0fJR05-KDdi14tqsuRdHrfAFpbs";
+        return "1983849094:AAGWyKDIO9jovVRn5BwVdKWSEfxo_qEw384";
     }
 
     @Override
@@ -36,16 +41,21 @@ public class Bot extends TelegramLongPollingBot {
         return "@Pump_up_your_brain_bot";
     }
 
-
     @Override
     public void onUpdateReceived(Update update) {
         String chat_Id = update.getMessage().getChatId().toString();
         SendMessage message = new SendMessage();
         message.setChatId(chat_Id);
+        System.out.println(update);
+
         if (UsersData.get(chat_Id) == null){
             UsersData.put(chat_Id, new UserData(chat_Id));
         }
         UserData userData = UsersData.get(chat_Id);
+        if (update.hasCallbackQuery()){
+            System.out.println("fghhgfvbhjg");
+            message.setText(processingCallbackQuery(update.getCallbackQuery()));
+        }
         if(update.getMessage()!=null && update.getMessage().hasText()) {
             String inputText = update.getMessage().getText();
             var usedTask = userData.GetUsedTasks();
@@ -64,19 +74,27 @@ public class Bot extends TelegramLongPollingBot {
                     var usedSequence = usedTask.get(Type.TypeTask.Sequence);
                     var sequence = sequenceCommand.getTask(usedSequence);
                     message.setText(sequence);
+                    message.setReplyMarkup(getInlineButton());
                     userData.SetCondition(State.Sequence, message.getText());
                 }
                 case "/issue" -> {
                     var usedIssue = usedTask.get(Type.TypeTask.Issue);
                     var issue = issueCommand.getTask(usedIssue);
                     message.setText(issue);
+                    message.setReplyMarkup(getInlineButton());
                     userData.SetCondition(State.Issue, message.getText());
                 }
                 case "/level" -> {
                     message.setText(levelCommand.getLevel(userData.GetLevel()));
                     userData.SetCondition(State.Level, message.getText());
                 }
-                case "/statistic" -> message.setText(statistic.getStatisticWithText(statistic.getCountTasks(userData.GetUsedTasks())));
+                case "/statistic" -> {
+                    var statistics = statistic.getCountTasks(userData.GetUsedTasks());
+                    userData.SetLastStatistic(statistics);
+                    message.setText(statistic.getStatisticWithText(
+                            statistics) + "\n\n" +
+                            statistic.getComparativeStatistic(UsersData, userData));
+                }
 
                 case "Подсказка" -> {
                     var condition = userData.GetCondition();
@@ -104,6 +122,8 @@ public class Bot extends TelegramLongPollingBot {
                                 message.setText(answer.answerString);
                                 if (answer.correctness) {
                                     userData.ChangeUsedTasks(Type.TypeTask.Issue, condition.task);
+                                    var statistics = statistic.getCountTasks(userData.GetUsedTasks());
+                                    userData.SetLastStatistic(statistics);
                                 }
                             }
                             case Sequence -> {
@@ -111,6 +131,8 @@ public class Bot extends TelegramLongPollingBot {
                                 message.setText(answer.answerString);
                                 if (answer.correctness) {
                                     userData.ChangeUsedTasks(Type.TypeTask.Sequence, condition.task);
+                                    var statistics = statistic.getCountTasks(userData.GetUsedTasks());
+                                    userData.SetLastStatistic(statistics);
                                 }
                             }
                             case Example -> {
@@ -119,6 +141,8 @@ public class Bot extends TelegramLongPollingBot {
                                 message.setText(answer.answerString);
                                 if (answer.correctness) {
                                     userData.ChangeUsedTasks(Type.TypeTask.Example, condition.task);
+                                    var statistics = statistic.getCountTasks(userData.GetUsedTasks());
+                                    userData.SetLastStatistic(statistics);
                                 }
                             }
                             case Level -> {
@@ -129,14 +153,42 @@ public class Bot extends TelegramLongPollingBot {
                     }
                     else message.setText("Не понимаю тебя( \nЧтобы просмотреть список команд введи /help.");
                     userData.SetCondition(State.NoState, "");
+                    System.out.println(userData.GetLastStatistic().countAllTasks);
                     UsersData.put(chat_Id, userData);
                 }
             }
+        }
+        else if (update.hasCallbackQuery()){
+            System.out.println("dfghjkl");
         }
         try {
             execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+    private InlineKeyboardMarkup getInlineButton(){
+        List<List<InlineKeyboardButton>> buttons = new ArrayList<>();
+        List<InlineKeyboardButton> buttonsFirst = new ArrayList<>();
+        InlineKeyboardButton clueButton = new InlineKeyboardButton();
+        clueButton.setText("Подсказка");
+        clueButton.setCallbackData("clueButton");
+        buttonsFirst.add(clueButton);
+        buttons.add(buttonsFirst);
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        markup.setKeyboard(buttons);
+        return markup;
+    }
+
+    private String processingCallbackQuery(CallbackQuery button){
+        String message = "Подсказки нет.";
+        String chat_id = button.getMessage().getChatId().toString();
+        if (button.getData().equals("clueButton")){
+            var condition = UsersData.get(chat_id).GetCondition();
+            var clue = Clue.GetClues();
+            message = clue.get(condition.task);
+        }
+        System.out.println(message);
+        return message;
     }
 }
--- src/Main/Statistic.java
+++ src/Main/Statistic.java
@@ -18,5 +18,26 @@ public class Statistic {
                 afterCount.countSequences - beforeCount.countSequences,
                 afterCount.countIssues - beforeCount.countIssues);
     }
+    public String getComparativeStatistic(Map<String, UserData> usersData, UserData thisUserData){
+        var countTasksThisUser = thisUserData.GetLastStatistic().countAllTasks;
+        double countUsersWithFewerQuantity = 0.0;
+        double countUsersWithSameQuantity = 0.0;
+        ArrayList<UserData> userData = new ArrayList<UserData>(usersData.values());
+        for (UserData user: userData){
+            var statOfAnotherUser = user.GetLastStatistic();
+            if (statOfAnotherUser.countAllTasks < countTasksThisUser){
+                countUsersWithFewerQuantity+=1;
+                System.out.println(9);
+            }
+            if (statOfAnotherUser.countAllTasks.equals(countTasksThisUser)){
+                countUsersWithSameQuantity+=1;
+            }
+        }
+        var percentFewer = usersData.size()>1 ? (countUsersWithFewerQuantity)/usersData.size()*100 : 100;
+        var percentSame = usersData.size()>1 ? (countUsersWithSameQuantity - 1)/usersData.size()*100 : 0;
+        System.out.println(countUsersWithFewerQuantity);
+        System.out.println((countUsersWithFewerQuantity/usersData.size()*100));
+        return String.format("Ты лучше %s%% пользователей; имеешь одинаковые баллы с %s%%. Всего пользователей - %s.", percentFewer, percentSame, usersData.size());
+    }
 }
 
