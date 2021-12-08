2d5544aec36ec3eb78abfe754d1ff1d1777e231a
kli44mar <71692931+kli44mar@users.noreply.github.com>
2021-12-08 12:44:15 +0500

    Api

--- src/main/java/Api.java
+++ src/main/java/Api.java
@@ -1,22 +1,30 @@
 package main.java;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 
 public class Api {
-    public ArrayList<String> GetExampleWithApi(String url) throws IOException {
+    public ArrayList<String> GetExampleWithApi(String u) throws IOException {
         var expression = new ArrayList<String>();
-        var connection = (HttpURLConnection) new URL(url).openConnection();
-        connection.setRequestMethod("GET");
-        var is = connection.getInputStream();
-        byte[] bytes = new byte[is.available()];
-        is.read(bytes);
-        var task = new String(bytes);
-        var t = task.split(",");
-        expression.add(t[3].split("\"")[3]);
-        expression.add(t[4].split(":")[1].replace("}", ""));
+        URL url = new URL(u);
+        URLConnection request = url.openConnection();
+        request.connect();
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        JsonObject rootObj = root.getAsJsonObject();
+        String message = rootObj.get("expression").getAsString();
+        String answer = rootObj.get("answer").getAsString();
+        expression.add(message);
+        expression.add(answer);
         return expression;
     }
 }
